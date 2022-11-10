10f3954f16de48d56f931bdf46640f1111ecf1ba
makhan0011 <112144798+makhan0011@users.noreply.github.com>
2022-11-10 22:43:21 +0000

    C

--- q1.cpp
+++ q1.cpp
@@ -2,26 +2,51 @@
 #include <iomanip>
 using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void swapTwoValues(int &, int &);
+void swapTwoValues(float &, float &);
+void getinput(int &, int &);
+void getinput(float &, float );
 
-int main()
+int main)
 {
 	int num1, num2;
 	float fnum1, fnum2;
-	getinput(num1, num2);
-	cout << " Before Swap" << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
-	swapTwoValues(num1, num2);
-	cout << " After Swap" << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
 
-	// -- Call overloaded function with float parameters
+	getinput(num1, num2);
+	cout << " Before Swap " << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
+	swapTwoValues(num1, num2);
+	cout << " After Swap " << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
+
 	getinput(fnum1, fnum2);
-	cout << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
+	cout << " Before Swap " << setw(5) << fnum1 << ' ' << fnum2 << setw(5) << endl;
+
 	swapTwoValues(fnum1, fnum2);
-	cout << setw(5) << fnum1 << setw(5) << fnum2 << setw(5) << endl;
+	cout << " After Swap " << setw(5) << fnum1 << ' ' << fnum2 << setw(5) << endl;
+
 }
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getinput(int &n1, int &n2) {
+	// cout << "Enter 2 integers: ";
+	cin >> n1 >> n2;
+	cout << endl;
+}
+
+void getinput(float &n1, float &n2) {
+	// cout << "Enter 2 floats: ";
+	cin >> n1 >> n2;
+	cout << endl;
+}
+
+void swapTwoValues(int &n1, int &n2) {
+	int temp;
+	temp = n1;
+	n1 = n2;
+	n2 = temp;
+}
+
+void swapTwoValues(float &n1, float &n2) {
+	float temp;
+	temp = n1;
+	n1 = n2;
+	n2 = temp;
+}
